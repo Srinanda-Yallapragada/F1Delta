b322d1dd7db56e6f837e09bc545c856876b245f4
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2025-04-13 03:43:18 -0400

    structual landing page with false data complete

---
+++ public/data/sources/localJSON.js
@@ -0,0 +1,11 @@
+export const track_locations_2025 = await fetch('/f1-circuits/championships/f1-locations-2025.json').then(res => res.json())
+export const locations = await fetch('/f1-circuits/f1-locations.json').then(res => res.json())
+
+// import locations from '/f1-circuits/f1-locations.json';
+
+/**
+ * @param {Array} location
+ */
+export function getIdFromLocation(location) {
+    return locations.find(e => e.location == location).id;
+}
---
+++ public/data/sources/openf1.js
@@ -0,0 +1,18 @@
+const OPENF1_API_URL = 'https://api.openf1.org/v1';
+
+
+export async function getDriverInfo(driver_number, session_key = 'latest') {
+    const res = await fetch(`${OPENF1_API_URL}/drivers?` + new URLSearchParams({
+        driver_number: driver_number,
+        session_key: session_key,
+    }))
+    return (await res.json())[0];
+}
+
+async function getCompletedRaces2025() {
+    const res = await fetch(`${OPENF1_API_URL}/sessions?session_name=Race&year=2025`);
+    return (await res.json()) 
+}
+
+
+export const completedRaces2025 = await getCompletedRaces2025();
