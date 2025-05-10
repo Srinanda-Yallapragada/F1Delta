0deacb180a3c19c71bfcecbcd9e1fb94df822afe
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2025-05-10 18:18:43 -0400

    removed javacript stuff

--- public/data/locations.js
+++
@@ -1,5 +0,0 @@
-import { getIdFromLocation as getLocObjFromName } from "./sources/localJSON.js";
-import { completedRaces2025 } from "./sources/api_calls.js";
-
-export const locationDetails2025 = completedRaces2025.map(race => getLocObjFromName(race.location))
-
--- public/data/sources/loadTrackResults.js
+++
@@ -1,7 +0,0 @@
-import { completedRaces2025 } from "./api_calls.js";
-
-import { MelbourneResults25 } from "./localJSON.js";
-
-export async function loadTrackResults(){
-    console.log(MelbourneResults25)
-}
